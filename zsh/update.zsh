6db997597180085ce21224d666e414243f2f10d6
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-05-26 09:40:54 +0200

    Remove unnecessary echo of installed variable
    Remove echo of installed variable before checking version.

--- zsh/update.zsh
+++ zsh/update.zsh
@@ -63,8 +63,6 @@ _term_check_remote() {
     "https://api.github.com/repos/$TERMINAL_REPO/commits/main" 2>/dev/null)" || return 0
   [[ -n "$remote" ]] || return 0
 
-  echo $installed
-
   installed=""
   [[ -f "$_term_version_file" ]] && installed="$(<"$_term_version_file")"
   if [[ -n "$installed" && "$remote" != "$installed" ]]; then
