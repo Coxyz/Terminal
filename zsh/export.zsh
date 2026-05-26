2701fb015d003d729946e0efa5d59a4f2430ca2c
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-05-26 09:32:23 +0200

    Remove VIRTUAL_ENV_DISABLE_PROMPT export

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -2,6 +2,4 @@
 # export.zsh — Variables d'environnement spécifiques à cette machine
 # =============================================================================
 
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-
 # Ajoute ici les exports locaux (non partagés)
