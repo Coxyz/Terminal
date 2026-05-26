0a47243176ba3ecb2839c7d378341e56218ef83c
Alexandre <75211316+Ekyoz@users.noreply.github.com>
2026-05-26 09:16:35 +0200

    Disable virtual environment prompt in export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -2,4 +2,6 @@
 # export.zsh — Variables d'environnement spécifiques à cette machine
 # =============================================================================
 
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
 # Ajoute ici les exports locaux (non partagés)
